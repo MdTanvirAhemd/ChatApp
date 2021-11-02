f9834d744ddb3310c6fe5d240eacb8077f910a21
Overlord <tanvir1005@student.sust.edu>
2021-11-02 21:42:56 +0600

    Initial commit

---
+++ app/src/main/java/com/ahmed/chatapp/FriendsActivity.java
@@ -0,0 +1,117 @@
+package com.ahmed.chatapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class FriendsActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private ArrayList<com.ahmed.chatapp.User> users;
+    private ProgressBar progressBar;
+    private com.ahmed.chatapp.UsersAdapter usersAdapter;
+    com.ahmed.chatapp.UsersAdapter.OnUserClickListener onUserClickListener;
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+
+    String myImageUrl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_friends);
+
+        progressBar = findViewById(R.id.progressBar);
+        users = new ArrayList<>();
+        recyclerView = findViewById(R.id.recycler);
+        swipeRefreshLayout = findViewById(R.id.swipeLayout);
+
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                getUsers();
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        onUserClickListener = new com.ahmed.chatapp.UsersAdapter.OnUserClickListener() {
+            @Override
+            public void onUserClicked(int position) {
+                startActivity(new Intent(FriendsActivity.this, com.ahmed.chatapp.MessageActivity.class)
+                        .putExtra("username_of_roommate",users.get(position).getUsername())
+                        .putExtra("email_of_roommate",users.get(position).getEmail())
+                            .putExtra("img_of_roommate",users.get(position).getProfilePicture())
+                        .putExtra("my_img",myImageUrl)
+
+                );
+
+            }
+        };
+
+        getUsers();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.profile_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.menu_item_profile){
+            startActivity(new Intent(FriendsActivity.this, com.ahmed.chatapp.Profile.class));
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void getUsers(){
+        users.clear();
+        FirebaseDatabase.getInstance().getReference("user").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    users.add(dataSnapshot.getValue(com.ahmed.chatapp.User.class));
+                }
+                usersAdapter = new com.ahmed.chatapp.UsersAdapter(users,FriendsActivity.this,onUserClickListener);
+                recyclerView.setLayoutManager(new LinearLayoutManager(FriendsActivity.this));
+                recyclerView.setAdapter(usersAdapter);
+                progressBar.setVisibility(View.GONE);
+                recyclerView.setVisibility(View.VISIBLE);
+
+                for (com.ahmed.chatapp.User user: users){
+                    if(user.getEmail().equals(FirebaseAuth.getInstance().getCurrentUser().getEmail())){
+                        myImageUrl = user.getProfilePicture();
+                        return;
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+}
